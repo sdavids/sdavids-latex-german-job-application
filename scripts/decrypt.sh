f3434f31c011c2884a50cede0052fc79334ac625
Sebastian Davids <sdavids@gmx.de>
2024-09-12 21:36:12 +0200

    feat: add sdavids-cv
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/decrypt.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# age
+#   Mac:
+#     brew install age
+#   Linux:
+#     sudo apt-get install age
+
+# sops
+#   Mac:
+#     brew install sops
+#   Linux:
+#     https://github.com/getsops/sops/releases
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+sops decrypt -i "${base_dir}/src/sdavids-cv.tex"
